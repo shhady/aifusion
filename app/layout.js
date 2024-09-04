658d856c1742df4b35859ef4596dcc4217c9d8e1
shhady <shhadyse@gmail.com>
2024-09-04 23:48:36 +0300

    first commit

--- app/layout.js
+++ app/layout.js
@@ -8,16 +8,12 @@ export const metadata = {
   title: "AIFusion",
   description: "Empowering businesses with cutting-edge web development, tailored digital solutions, and seamless integration of AI technologies to drive growth and innovation.",
   icons: {
-    icon: [
-      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-    ],
+    icon: "/favicon-32x32.png", // Simplified icon property
     apple: "/apple-touch-icon.png",
-    maskIcon: { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+    maskIcon: { href: "/safari-pinned-tab.svg", color: "#5bbad5" },
   },
   manifest: "/site.webmanifest",
 };
-
 export const viewport = {
   themeColor: "#ffffff",
 };
